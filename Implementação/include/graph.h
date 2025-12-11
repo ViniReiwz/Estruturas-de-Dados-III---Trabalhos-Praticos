ed569b216bf4334412299e23448f425a3cfb4ed3
ViniReiwz <vinireisg@usp.br>
2025-12-11 17:42:49 -0300

    Funções de criação e alocação de memória para o grafo estruturadas

--- Implementação/include/graph.h
+++ Implementação/include/graph.h
@@ -13,22 +13,23 @@
 #include "all.h"
 
 // Estrutura de dados que representa a lista de adjacências do grafo
-typedef struct _adjacence_list
+typedef struct _adjacence_node
 {
 
     char* nomeUsuarioqueESeguido;   // Nome do usuário do vértice adjacente
     char DataInicioQueSegue [10];   // Data que se iniciou a relação entre os usuários
     char DataFimQueSegue [10];      // Data que se terminou a relação entre os usuários
     char grauAmizade;               // Grau de amizade entre os usuários
-    struct _adjacence_list* next;   // Próximo elemento na lista (Próxima adjacência do vértice atual)
+    struct _adjacence_node* next;   // Próximo elemento na lista (Próxima adjacência do vértice atual)
+    struct _adjacence_node* ant;    // Elemento anterior ao atual
 
-}ADJ_LIST;
+}ADJ_NODE;
 
 // Represent um vértice qualquer do grafo, contendo o nome de usuário e sua lista de adjacências
 typedef struct _graph_vertex
 {
     char* nomeUsuarioqueSegue;  // Nome do referente usuário
-    ADJ_LIST* adj_head;         // Início da lista de adjacências do mesmo
+    ADJ_NODE* adj_head;         // Início da lista de adjacências do mesmo
 
 }VERTEX;
 
--- Implementação/src/graph.c
+++ Implementação/src/graph.c
@@ -8,3 +8,38 @@
 //Arquivo 'graph.c', contém funções que trabalham com a criação e manipulação do grafo
 
 #include "all.h"
+
+
+ADJ_NODE* create_adj_node()
+{
+    ADJ_NODE* adj = (ADJ_NODE*)calloc(1,sizeof(ADJ_NODE));
+    strcpy(adj->DataFimQueSegue,"---------");
+    strcpy(adj->DataInicioQueSegue,"--------");
+    adj->grauAmizade = '$';
+    adj->next = NULL;
+    adj->nomeUsuarioqueESeguido = NULL;
+
+    return adj;
+}
+
+VERTEX* create_vertex()
+{
+    VERTEX* vertex = (VERTEX*)calloc(1,sizeof(VERTEX));
+    vertex->adj_head = NULL;
+    vertex->nomeUsuarioqueSegue = NULL;
+
+    return vertex;
+}
+
+GRAPH* create_graph(int vertices_n)
+{
+    GRAPH* graph = (GRAPH*)calloc(1,sizeof(GRAPH));
+    graph->vertices_n = vertices_n;
+    graph->vertices_array = (VERTEX*)calloc(vertices_n,sizeof(VERTEX));
+
+    for(int i = 0; i < vertices_n; i ++)
+    {
+        graph->vertices_array[i] = *create_vertex();
+    }
+    return graph;
+}
